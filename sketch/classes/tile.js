578c8ad8dfa7599cdcda0b257bf90704b81224fb
Santiago Reyes <sreyeso@unal.edu.co>
2023-03-29 20:26:44 -0500

    Parte 2, dejar lo necesario

--- sketch/classes/tile.js
+++ sketch/classes/tile.js
@@ -1,68 +1,19 @@
-class casilla {
-  constructor(tipo, numero, objeto) {
-    //Definidas por constructor
-    this.n = numero;
-    this.objeto = objeto;
-    this.tipo = tipo;
+class Tile {
 
-    //Valor por defecto (definida por inicializacion)
-    this.color = "white";
-
-    this.inicializar();
+  constructor(type) {
+    this.color;
+    this.initialize(type);
   }
 
-  inicializar() {
-    switch (this.tipo) {
-      case 0: //Casilla limite (limite del nivel)
-        this.color = "lightsteelblue";
-        if (this.n != 0) {
-          this.n = 0;
-        }
-        break;
-      case 1: //Casilla vacía (cualquier dirección)
-        this.color = "white";
-        if (this.n != 0) {
-          this.n = 0;
-        }
-        break;
-      case 2: //Casilla de inicio
-        this.color = "darkkhaki";
-        if (this.n != 0) {
-          this.n = 0;
-        }
-        break;
-      case 3: //Casilla final
-        this.color = "chartreuse";
-        if (this.n != 0) {
-          this.n = 0;
-        }
-        break;
-      case 4: //Casilla completada
-        this.color = "aquamarine";
-        break;
-      case 5: //Casilla azul (tecla z)
-        this.color = "lightskyblue";
-        if (this.n == 0) {
-          this.n = 1;
-        }
-        break;
-      case 6: //Casilla roja (tecla x)
-        this.color = "firebrick";
-        if (this.n == 0) {
-          this.n = 1;
-        }
-        break;
-      default:
-        this.color = "purple";
-    }
+  initialize(type) {
+    if      (type==0) {this.color = "lightsteelblue";}
+    else if (type==1) {this.color = "white";}
+    else if (type==2) {this.color = "darkkhaki";}
+    else if (type==3) {this.color = "chartreuse";}
+    else if (type==4) {this.color = "aquamarine";}
+    else if (type==5) {this.color = "lightskyblue";}
+    else if (type==6) {this.color = "firebrick";}
+    else              {this.color = "purple";}
   }
-  completar() {
-    this.tipo = 4;
-    this.inicializar();
-    lvl.compl += 1;
-  }
-  vaciar() {
-    this.tipo = 1;
-    this.inicializar();
-  }
+
 }
--- sketch/config.js
+++ sketch/config.js
@@ -1,18 +1,6 @@
-//Variables gráficas
-let width;        //Ancho de la ventana
-let height;       //Largo de la ventana
-let x;            //Imprimir el jugador en la coordenada
-let y;            //Imprimir el jugador en la coordenada
 
-//Variables del juego
-let game='0';     //0-menu inicial 1-juego 2-editor 3-exit
-let px;           //Coordenada en x del jugador 
-let py;           //Coordenada en y del jugador
-let gx;           //Coordenada en x de la meta
-let gy;           //Coordenada en y de la meta
-let lvl;          //Objeto donde se guarda el nivel actual (modo jugar)
+/*
 let pc;           //Diccionario donde se guarda el color del jugador
-let combo="Bien"; //Control del movimiendo valido
 let clvl;         //Objeto donde se guarda el JSON para carga de nivel
 let nlvl=0;       //Contador de nivel para carga sequencial de niveles
 let lvls;         //Pre-carga de los niveles de JSON a Objeto
@@ -36,3 +24,4 @@ let score;
 let exportlvl;
 let sldrtiming;
 let timingtxt;
+*/
