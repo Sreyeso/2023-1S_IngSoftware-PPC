8b210fdf12ca74a2ffa84e8b12a62c07f026129e
Infernostx <termiguel@gmail.com>
2023-04-05 20:18:33 -0500

    ez sketch

---
+++ src/pages/sketch.tsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import dynamic from 'next/dynamic';
+import p5Types from "p5"; //Import this for typechecking and intellisense
+//import Sketch from "react-p5";
+// Will only import `react-p5` on client-side
+const Sketch = dynamic(() => import("react-p5").then((mod) => {
+
+    // importing sound lib only after react-p5 is loaded
+    require('p5/lib/addons/p5.sound');
+
+    // returning react-p5 default export
+    return mod.default
+}), {
+    ssr: false
+});
+//import Sketch from "react-p5";
+
+
+export default class App extends Component {
+    setup = (p5, canvasParentRef) => {
+        p5.createCanvas(window.innerWidth, window.innerHeight).parent(
+            canvasParentRef
+        );
+        p5.frameRate(this.fr);
+        // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
+    };
+    draw = p5 => {
+        p5.fill(234, 31, 81);
+        p5.noStroke();
+        p5.rect(50, 50, 250, 250);
+        p5.fill(255);
+        p5.textStyle(p5.BOLD);
+        p5.textSize(140);
+        p5.text("p5*", 60, 250);
+    };
+
+    render() {
+        return <Sketch setup={ this.setup } draw = { this.draw } />;
+    }
+}
