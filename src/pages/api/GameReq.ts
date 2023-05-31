012cc7fac7ba7a14d5616a0b4b7ddb01f864b514
Infernostx <termiguel@gmail.com>
2023-05-31 11:07:54 -0500

    close db in API

--- src/pages/api/GameReq.ts
+++ src/pages/api/GameReq.ts
@@ -14,32 +14,44 @@ export async function connection(body:any){
     await UDO.setScore("bingus",parseInt(body.score));
     //let userSkin=await 
     //retorno Objid, id in array, name ...
-    return Promise.resolve(); // Resolve the promise when the operations are completed
+    //machete gaming
+    if(true){
+      DB.end();
+    }
+    else{
+      console.log("ERROR FETCHING USER DATA")
+    }
+    return DB;
   } catch (e) {
     console.error(e)
-    return Promise.reject(e); // Reject the promise if an error occurs
+    return null;
   }
   
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const requestMethod = req.method;
+  console.log(req.body);
   const body = req.body;  
   switch (requestMethod) {
     case 'POST':
-      res.status(200).json({ message: `You submitted the following data: ss` });
-      break;
+      res.status(200).json({ message: `You submitted the following data: ss` })
     case 'PUT':
-      connection(body)
-      .then(() => {
-        res.status(200).json({ message: `Entry successfully updated` });
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).json({ message: `Error updating entry` });
-      });
-      break;
+      //const _DB=connection(body);
+      connection(body);
+      res.status(200).json({ message: `Entry successfully updated` })
+      /*
+      //added connection close, but it lacks proper handling of errors and conditions
+      if(res.statusCode=200){
+        _DB.end();
+      }
+      else{
+        console.log("ERROR FETHCING USER DATA")
+      }
+      */
+
+    // handle other HTTP methods
     default:
-      res.status(401).json({ message: 'Api gaming' });
+      res.status(200).json({ message: 'Api gaming'})
   }
 }
