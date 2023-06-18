843a7096e664796de75b3f872d1c914b2049ab3a
Infernostx <termiguel@gmail.com>
2023-06-18 18:10:39 -0500

    buttons

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -148,7 +148,7 @@ export default class App extends Component<Clients> {
 
         return (
             <div>
-              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'top', height: '70vh' }}>
                 <Sketch 
                   preload={this.preload}
                   setup={this.setup}
