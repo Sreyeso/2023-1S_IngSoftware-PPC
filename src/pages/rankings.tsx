72ecac449cf06962b71280334fc9c8a4ed9a3dae
DysDan <dtobarl@unal.edu.co>
2023-06-19 21:30:59 -0500

    Update rankings.tsx

--- src/pages/rankings.tsx
+++ src/pages/rankings.tsx
@@ -44,7 +44,7 @@ import Clients from "@/lib/models/user";
             ranks[i] = {UserName: 'Vacio', Region: 'None', HiScore: 0, CurrentAspect: ['default_ppc.png','none.png']};
           }
         }
-        ranks[9] = ranks[i] = {UserName: 'Vacio', Region: 'None', HiScore: 0, CurrentAspect: ['default_ppc.png','none.png']};
+        ranks[9] = {UserName: 'Vacio', Region: 'None', HiScore: 0, CurrentAspect: ['default_ppc.png','none.png']};
       }
       return {
         props: { ranks,},
